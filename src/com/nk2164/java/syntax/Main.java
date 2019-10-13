6eb022817cd1894ccf65e1d0df0f79a2394b4c85
nk2164 <nk2164@live.com>
2019-10-13 13:49:06 -0500

    Added if case and for loops

--- src/com/nk2164/java/syntax/Main.java
+++ src/com/nk2164/java/syntax/Main.java
@@ -3,7 +3,7 @@ package com.nk2164.java.syntax;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 //		int i = 1;
 //		int j;
 //		j = 2;
@@ -15,7 +15,7 @@ public class Main {
 //		
 //		double d = 123.4;
 //		float f = 1.2f;
-		
+
 //		int i1 = 100;
 //		int i2 = 51;
 //		
@@ -37,7 +37,7 @@ public class Main {
 //		
 //		int i5 = i3%i4;
 //		System.out.println(i5);
-		
+
 //		int i1 = 100;
 //		int i2 = 51;
 //		
@@ -49,32 +49,107 @@ public class Main {
 //		char c = 'M';
 //		System.out.println(c);
 //		System.out.println(b);
-		
+
 //		int[] j = {1,2,3,4,5};
 //		System.out.println(j[0]);
 //		
 //		char[] myName = {'M','a','t','t'};
 //		System.out.println(myName);
 
-//		int[] j = {11,22,33,44,55};
-//		
+//		int[] j = { 11, 22, 33, 44, 55 };
+//
 //		for (int count = 0; count < j.length; count++) {
 //			System.out.println(j[count]);
 //		}
-		int[] myArray = {11,14,15,75,23,75,43,23,53,94};
+
+//		int i = 0;
+//		
+//		while (i < 5) {
+//			System.out.println("Hello!");
+//			i++;
+//		}
+//		
+//		int[] myArray = { 21, 34, 54, 25, 75, 23, 53, 94 };
+//		int[] myArray = { 11, 14, 14, 15, 75, 23, 53, 94 };
+//
+//		int numberUsed = 0;
+//		int total = 500;
+//
+//		do {
+//			total += myArray[numberUsed];
+//			numberUsed++;
+//		} while (total < 100);
+//
+//		System.out.println("Total : " + total);
+//		System.out.println("numberUsed : " + numberUsed);
 		
-		int numberUsed=0;
-		int total = 0;
+//		String myName = "Johnny Ive";
+//		int spacePosition = myName.indexOf(" ");
+//		System.out.println(spacePosition);
+//		
+//		System.out.println(myName.startsWith("John"));
+//		
+//		String myFirstName = myName.substring(0, spacePosition).toUpperCase();
+//		System.out.println(myFirstName);
 		
-		int i = 0;
-		while(total < 100)
-		{
-			total += myArray[numberUsed];
-			numberUsed++;
+//		int i = 6;
+//		
+//		System.out.println("Starting");
+//		
+//		if (i < 7) {
+//			System.out.println("It's low !");
+//		} else if (i > 7) {
+//			System.out.println("It's high !");
+//		} else {
+//			System.out.println("Its a match !");
+//		}
+//		
+//		System.out.println("Ending");
+		
+//		int i = 7;
+//		
+//		switch (i) {
+//		case 6:
+//			System.out.println("Its a 6!");
+//			break;
+//		case 7:	
+//			System.out.println("Its a 7!");
+//			break;
+//		default:
+//			System.out.println("It must be something else!");
+//			break;
+//		}
+		
+//		int dayNumber = 1;
+//		
+//		switch (dayNumber) {
+//		case 0:
+//			System.out.println("Saturday");
+//			break;
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//		default:
+//			System.out.println("another day");
+//			break;
+//		}
+		
+//		int first = 7;
+		int first = 8;
+		int second = 7;
+		
+//		if (second > first && first == 7) {
+		if (second > first || first == 7) {
+//			System.out.println("both are true!");
+			System.out.println("either are true!");
+		} else {
+//			System.out.println("at least one is false!");
+			System.out.println("both are false!");
 		}
 		
-		System.out.println("Total : " + total);
-		System.out.println("numberUsed : " + numberUsed );
 	}
 
 }
