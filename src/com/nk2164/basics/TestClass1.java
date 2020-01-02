a85da933568c5eafdd4b8a074aab935c6e40290d
nk2164 <nk2164@live.com>
2020-01-01 20:21:45 -0600

    numbering system octal

--- src/com/nk2164/basics/TestClass1.java
+++ src/com/nk2164/basics/TestClass1.java
@@ -4,12 +4,12 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		int x = 017; // in octal
+		int x = 0237; // in octal
 		int y = 0b11111111; //in binary
 		int z = 0b00001111; // in binary
 		char a = '\u0003';
 		
-		System.out.println(a);
+		System.out.println(x);
 	}
 }
 
