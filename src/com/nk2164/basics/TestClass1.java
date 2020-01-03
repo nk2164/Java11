17d54e7f28a2d5f53c4f8dda1f9a78eced9d9bdc
nk2164 <nk2164@live.com>
2020-01-02 23:20:17 -0600

    Primitive assignment

--- src/com/nk2164/basics/TestClass1.java
+++ src/com/nk2164/basics/TestClass1.java
@@ -2,14 +2,31 @@ package com.nk2164.basics;
 
 public class TestClass1 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+//		System.out.println("Hello World!");
 		
-		int x = 0237; // in octal
-		int y = 0b11111111; //in binary
-		int z = 0b00001111; // in binary
-		char a = '\u0003';
+//		int x = 0237; // in octal
+//		int y = 0b11111111; //in binary
+//		int z = 0b00001111; // in binary
+//		char a = '\u0003';
+//		char newline = '\n';
+//		
+//		System.out.println(x);
+//		System.out.println(a);
+//		System.out.println("Hello" + newline + "World");
 		
-		System.out.println(x);
+		byte b = 10; // b is 8 bits 
+		char c = 'x'; // c is 16 bits 
+		short s = 300; // c is 16 bits 
+		int i; // i is 32 bits 
+		long l; // l is 64 bits 
+		float f;// f is 32 bits 
+		double d; // d is 64 bits 
+		
+		i = b; 
+		i = s; 
+		l = i; 
+		f = i; 
+		d = f; 
+	
 	}
 }
-
