e0b51a4c5f4d3199ea634012b6603511a3a1c36a
nk2164 <nk2164@live.com>
2020-01-04 14:52:11 -0600

    casting and assignment

--- src/com/nk2164/basics/TestClass1.java
+++ src/com/nk2164/basics/TestClass1.java
@@ -2,50 +2,11 @@ package com.nk2164.basics;
 
 public class TestClass1 {
 	public static void main(String[] args) {
-//		System.out.println("Hello World!");
+		int i = Integer.MAX_VALUE;
+		float f = Float.MAX_VALUE;
 		
-//		int x = 0237; // in octal
-//		int y = 0b11111111; //in binary
-//		int z = 0b00001111; // in binary
-//		char a = '\u0003';
-//		char newline = '\n';
-//		
-//		System.out.println(x);
-//		System.out.println(a);
-//		System.out.println("Hello" + newline + "World");
-		
-//		byte b = 10; // b is 8 bits 
-//		char c = 'x'; // c is 16 bits 
-//		short s = 300; // c is 16 bits 
-//		int i; // i is 32 bits 
-//		long l; // l is 64 bits 
-//		float f;// f is 32 bits 
-//		double d; // d is 64 bits 
-//		
-//		i = b; 
-//		i = s; 
-//		l = i; 
-//		f = i; 
-//		d = f;
-		
-		int i = 10; 
-		char c = (char) i; // explicitly casting i to char 
-		long l = 100; 
-		i = (int) l; // explicitly casting l to int 
-		byte b = (byte) i; // explicitly casting i to byte 
-		double d = 10.0; 
-		float f = (float) d; // explicitly casting d to float
+		f = i;  // integer to float is ok
+		i = (int) f; // float to integer requires explicit cast
+	}
 
-		 
-		
-//		final int i = 10;
-//		byte j = i;
-//		
-//		someMethod(i);
-	
-	}
-	
-	public static void someMethod(byte j) {
-		System.out.println(j);
-	}
 }
