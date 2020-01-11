d8000880563ee2126546ac113d58555258ac57bb
nk2164 <nk2164@live.com>
2020-01-11 02:01:41 -0600

    StringBuilder more examples

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -4,10 +4,11 @@ public class TestClass {
 
 	public static final boolean DEBUG = false;
 	
-	public static Bird b;
+	public static Bird b = new Bird();
 	public static int i = 20;
 	
 	public static void main(String[] args) throws Exception {
+		
 //		int x = 0;
 //		if (x == 0) {
 //			throw new Exception();
@@ -25,8 +26,8 @@ public class TestClass {
 //		System.out.println(str1 == str2);
 //		System.out.println(str1 == str3);
 		
-        System.out.println(10 * 20 + "Java");
-        System.out.println("Java" + 10 * 20);
+//      System.out.println(10 * 20 + "Java");
+//      System.out.println("Java" + 10 * 20);
 //		
 //		StringBuilder s1 = new StringBuilder("abc");
 //		StringBuilder s2 = new StringBuilder(" abc".stripLeading());
@@ -35,6 +36,11 @@ public class TestClass {
 //		System.out.println(s2.toString());
 //		
 //		System.out.println(s1 == s2);
+        
+        StringBuilder sb = new StringBuilder("Hello ");
+        sb.append(true).append(12.3).append(b);
+        
+        System.out.println(sb);
 	}
 
 	public void method() {
