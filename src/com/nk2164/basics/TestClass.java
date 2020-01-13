9352c845d2e848d2b0cd64ce9c00d652616fb8cc
nk2164 <nk2164@live.com>
2020-01-12 22:25:26 -0600

    more changes

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int i1 = -4; // 
-		System.out.println(Integer.toBinaryString(i1));
-		int i2 = 1;  // 
-		int i3 = i1 >>> i2;
-		System.out.println(Integer.toBinaryString(i3));
-		System.out.println(i3);
+//		int i1 = -4; // 
+//		System.out.println(Integer.toBinaryString(i1));
+//		int i2 = 1;  // 
+//		int i3 = i1 >>> i2;
+//		System.out.println(Integer.toBinaryString(i3));
+//		System.out.println(i3);
+//		
+//		byte b1 = -4; // 
+//		System.out.println(Integer.toBinaryString(b1));
+//		byte b2 = 1;  // 
+//		byte b3 = (byte) (b1 >>> b2);
+//		System.out.println(Integer.toBinaryString(b3));
+//		System.out.println(b3);
 		
-		byte b1 = -4; // 
-		System.out.println(Integer.toBinaryString(b1));
-		byte b2 = 1;  // 
-		byte b3 = (byte) (b1 >>> b2);
-		System.out.println(Integer.toBinaryString(b3));
-		System.out.println(b3);
+		int i = 1; 
+		i = i++; 
+		System.out.println(i);
+
 	}
 
 }
