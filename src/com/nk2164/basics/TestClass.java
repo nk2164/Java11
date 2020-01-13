2e7d696d51e97236e0e3d6faa670a9fc57f29f56
nk2164 <nk2164@live.com>
2020-01-12 22:04:19 -0600

    expressions and statements

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -5,32 +5,19 @@ import java.util.Scanner;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int a = 10;
-		Integer b = 20;
-		char ch = 'a';
-		Double d = 10.0;
-		Boolean flag = false;
-
-		System.out.println(a == b); // comparing an int with an Integer,
-									// prints false because 10 is not equal to 20
-
-		System.out.println(a == 10.0); // comparing an int with a double,
-										// prints true because Java considers 10 and 10.0 as equal
-
-		System.out.println(a == ch); // comparing an int with a char, prints false
-										// because 10 is not equal to 'a'
-
-		System.out.println(97 == ch); // comparing an int with a char, prints true because
-										// int value of 'a' is indeed 97
-
-		System.out.println(a != d); // comparing an int with a Double, prints false
-									// because a and d have the same value
-
-		System.out.println(a != 10); // comparing two ints, prints false because a is 10
-
-		System.out.println(false != flag); // comparing a boolean with a Boolean,
-											// prints false because flag is false
-
+		int i1 = -4; // 
+		System.out.println(Integer.toBinaryString(i1));
+		int i2 = 1;  // 
+		int i3 = i1 >>> i2;
+		System.out.println(Integer.toBinaryString(i3));
+		System.out.println(i3);
+		
+		byte b1 = -4; // 
+		System.out.println(Integer.toBinaryString(b1));
+		byte b2 = 1;  // 
+		byte b3 = (byte) (b1 >>> b2);
+		System.out.println(Integer.toBinaryString(b3));
+		System.out.println(b3);
 	}
 
 }
