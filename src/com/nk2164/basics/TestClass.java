e18a1c03bb36e989ba1570a47637cb6d806c5692
nk2164 <nk2164@live.com>
2020-01-13 08:15:47 -0600

    more string concatenation

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -1,34 +1,13 @@
 package com.nk2164.basics;
 
-import java.util.Scanner;
-
 public class TestClass {
 
 	public static void main(String[] args) {
-//		int i1 = -4; // 
-//		System.out.println(Integer.toBinaryString(i1));
-//		int i2 = 1;  // 
-//		int i3 = i1 >>> i2;
-//		System.out.println(Integer.toBinaryString(i3));
-//		System.out.println(i3);
-//		
-//		byte b1 = -4; // 
-//		System.out.println(Integer.toBinaryString(b1));
-//		byte b2 = 1;  // 
-//		byte b3 = (byte) (b1 >>> b2);
-//		System.out.println(Integer.toBinaryString(b3));
-//		System.out.println(b3);
-		
-//		int i = 1; 
-//		i = i++; 
-//		System.out.println(i);
-		
-		Double d = 1.0;
-		String s = "2";
-		
-		String s3 = d + s;
-		
-		System.out.println(s3);
-	}
 
+		TestClass tc = null; 
+		String sc = "Hello ";
+		System.out.println(sc += tc);
+
+	}
+	
 }
