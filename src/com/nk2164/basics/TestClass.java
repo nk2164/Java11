8de4ae8cc0add8f07681083fc5a1f55607d99312
nk2164 <nk2164@live.com>
2020-01-13 22:29:16 -0600

    operator promotion rules

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,11 +3,12 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-
-		TestClass tc = null; 
-		String sc = "Hello ";
-		System.out.println(sc += tc);
-
+		byte b1 = 1;
+		byte b2 = ++b1; // result of + + b1 will be a byte
+		b2 = b1--; // result of b1-- will be a byte
+		b1 *= b2; // result will be a byte
+		double d = 1.0;
+		float f = 2.0f;
+		f += d; // result of will be a float
 	}
-	
 }
