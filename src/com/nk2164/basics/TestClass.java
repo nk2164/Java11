e8a19e58f1bb4e98b3b1032324fdf2cf93cabb4d
nk2164 <nk2164@live.com>
2020-01-16 01:26:18 -0600

    if else constructs

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,42 +3,25 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int m = 50; 
-		
-		int n = ++ m;  // n = 51, m = 51
-		
-		int o = m--; // o = 51, m = 50
-		
-		int p = --o + m--; // p = 50 + 50 = 100 , o = 50, m = 49 
-		
-		int x = m < n? n < o? o < p? p:o:n:m;
-		
-		System.out.println(x);
-		// 49 < 51 ? (51 < 50 ? (50 < 100 ? 100:50);51);49) 
-		// 49 < 51 ? 51;49)
-		
-		
-		int k = 4; 
-		boolean flag = k++ == 5;
-		System.out.println(flag);
-		flag = !flag;
-		
-		
-		byte b = 1; 
-		//b = b << 1; 
-		int c = b << 1; 
-		byte d = b; 
-		byte e = 0; 
-		e += b;
+		int value = 3;
 
-		String s = "a"; 
-		String[] sa = { "a", s, s.substring( 0, 1), new String(" a"), "" + 'a' }; 
+		if (value == 0)
+			if (value == 1)
+				System.out.println(" b");
+			else
+				System.out.println(" c");
+		else
+			System.out.println("did not go in");
 		
-		for( int i = 0; i < sa.length; i ++){ 
-			System.out.println( i); 
-			System.out.println( s == sa[ i]); 
-			System.out.println( s.equals( sa[ i])); 
-			}
+		
+		value = 3; 
+		if( value == 0) 
+			if( value == 1) 
+				System.out.println(" b"); 
+			else 
+				System.out.println(" c"); 
+		else 
+			System.out.println(" d");
 
 
 	}
