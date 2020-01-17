bedec41e65f97d9fa1aa7169eae509905f9ba326
nk2164 <nk2164@live.com>
2020-01-16 21:42:44 -0600

    if else conditionals

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,14 +3,16 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-		Object data;
-		if ((data = getData()) == null) {
-			System.out.println("Hello There!!");
+		int x = 0;
+		if (x++ > 0) { // line 2
+			x--; // line 3
 		}
+
+		if (++x == 2) { // line 6
+			x++; // line 7
+		}
+
+		System.out.println(x);
 	}
-	
-	public static Object getData() {
-		System.out.println("I got called");
-		return null;
-	}
+
 }
