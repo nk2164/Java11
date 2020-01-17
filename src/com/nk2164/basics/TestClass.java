b4f52580cb640aae0554d620064820c6bb6e2167
nk2164 <nk2164@live.com>
2020-01-17 00:49:53 -0600

    switch statement

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,8 +3,23 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int a = 3 , b = 3;
-		double d = a == b? 5 : 10.0;
+
+		int a = 3;
+		int b = 3;
+		
+		String str = a == b? "5" : "hello";
+		System.out.println(str);
+		
+		String s = "12";
+		
+		switch (s) {
+		case "1" + "2":
+			System.out.println("I came here");
+			break;
+
+		default:
+			break;
+		}
 	
 	}
 
