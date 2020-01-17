9620023d776974cba9d270d2a0e4c890008e3a85
nk2164 <nk2164@live.com>
2020-01-16 21:34:50 -0600

    if condition confusing questions.

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,26 +3,14 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int value = 3;
-
-		if (value == 0)
-			if (value == 1)
-				System.out.println(" b");
-			else
-				System.out.println(" c");
-		else
-			System.out.println("did not go in");
-		
-		
-		value = 3; 
-		if( value == 0) 
-			if( value == 1) 
-				System.out.println(" b"); 
-			else 
-				System.out.println(" c"); 
-		else 
-			System.out.println(" d");
-
-
+		Object data;
+		if ((data = getData()) == null) {
+			System.out.println("Hello There!!");
+		}
+	}
+	
+	public static Object getData() {
+		System.out.println("I got called");
+		return null;
 	}
 }
