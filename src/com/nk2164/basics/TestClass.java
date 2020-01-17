ba7338b138d7394385c7b7c2241998d2151a088b
nk2164 <nk2164@live.com>
2020-01-17 00:19:53 -0600

    ternary op with different numeric types
    if the 3rd and 4th operand are of different type the resulting type is
    the wider type

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -3,20 +3,9 @@ package com.nk2164.basics;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int x = 0;
-
-		if (x++ > 0) { // line 2
-			x--; // line 3
-		}
-
-		if (++x == 2) { // line 6
-			x++; // line 7
-		}
-
-		System.out.println(x);
-
-		String y;
-		System.out.println(y = (x == 3) ? "First" : "second");
+		int a = 3 , b = 3;
+		double d = a == b? 5 : 10.0;
+	
 	}
 
 }
