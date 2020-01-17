ffff668fcf717656223cf266f2f3f7d3142a75da
nk2164 <nk2164@live.com>
2020-01-17 17:06:34 -0600

    end of if-else , case constructs

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -2,22 +2,54 @@ package com.nk2164.basics;
 
 public class TestClass {
 
+	static int count = 5, sum = 0;
+
 	public static void main(String[] args) {
 
-		int i = 0;
+		doStuff();
 
-		switch (args[0]) {
-		default:
-			i = i + 3;
-		case "2":
-			i = i + 2;
-		case "0":
-			break;
-		case "1":
-			i = i + 1;
+		int a = 0, b = 0, c = 10, d = 0;
+
+		boolean flag = true;
+
+		if (a == b)
+
+			if (c == 10) {
+				if (d > a) {
+				} else {
+				}
+
+				if (flag)
+					System.out.println("I came here");
+				else
+					System.out.println("No i came here");
+			} else if (flag == false)
+				System.out.println("");
+			else if (a + b < d) {
+				System.out.println("");
+			} else
+				System.out.println("");
+		else
+			d = b;
+
+	}
+
+	public static void doStuff() {
+		while (count > 0) {
+			switch (count) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				sum += count--;
+				break;
+			default:
+				break;
+			}
 		}
-
-		System.out.println(" i is " + i);
+		System.out.println("Sum is " + sum);
 	}
 
 }
