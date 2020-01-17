58430646f4ba614aebc7bcf80fa4133c603bc606
nk2164 <nk2164@live.com>
2020-01-17 01:12:16 -0600

    switch statements

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -4,23 +4,20 @@ public class TestClass {
 
 	public static void main(String[] args) {
 
-		int a = 3;
-		int b = 3;
-		
-		String str = a == b? "5" : "hello";
-		System.out.println(str);
-		
-		String s = "12";
-		
-		switch (s) {
-		case "1" + "2":
-			System.out.println("I came here");
-			break;
+		int i = 0;
 
+		switch (args[0]) {
 		default:
+			i = i + 3;
+		case "2":
+			i = i + 2;
+		case "0":
 			break;
+		case "1":
+			i = i + 1;
 		}
-	
+
+		System.out.println(" i is " + i);
 	}
 
 }
