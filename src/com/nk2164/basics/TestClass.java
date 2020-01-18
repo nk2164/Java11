9bcb4474bd6a547e6acecfb94242bb6cce747212
nk2164 <nk2164@live.com>
2020-01-17 23:36:39 -0600

    finding bob

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -1,5 +1,6 @@
 package com.nk2164.basics;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class TestClass {
@@ -16,30 +17,44 @@ public class TestClass {
 		Account act2 = new Account("12");
 		Account act3 = new Account("13");
 		Account act4 = new Account("14");
-		
+
 		BankAccount ba = new BankAccount();
 		ba.addAccount(act1);
 		ba.addAccount(act2);
 		ba.addAccount(act3);
 		ba.addAccount(act4);
-		
+
 //		Iterator<Account> baIterator = ba.iterator();
 //		Account act;
 //		while(baIterator.hasNext()) {
 //			act = baIterator.next();
 //			System.out.println(act.toString());
 //		}
-		
+
 		// same code above with for each
-		for (Account act : ba) {
-			System.out.println(act.toString());
-			if (act.equals(act3)) {
-				System.out.println("I'm out after acount 13");
-				break;
+//		for (Account act : ba) {
+//			System.out.println(act.toString());
+//			if (act.equals(act3)) {
+//				System.out.println("I'm out after acount 13");
+//				continue;
+//			}
+//		}
+
+//		ba.forEach(System.out :: println);
+
+		String[][] groups = { { "ally", "bob", "charlie"}, 
+				              { "bob", "alice", "boone"}, 
+				              { "chad", "dave", "elliot"} };
+
+		MY_OUTER_LOOP: for (int i = 0; i < groups.length; i++) {
+			for (String name : groups[i]) {
+				System.out.println(" Checking " + name);
+				if ("bob".equals(name)) {
+					System.out.println(" Found bob in Group " + (i+1) + " " + Arrays.toString(groups[i]));
+					continue MY_OUTER_LOOP;
+				}
 			}
 		}
-		
-//		ba.forEach(System.out :: println);
 	}
 
 	public static void doStuff1() {
@@ -52,33 +67,38 @@ public class TestClass {
 
 		System.out.println(" Value of i after the loop is " + i);
 	}
-	
+
 	private static void dostuff2() {
-		int i = 4; 
-		
-		while(i-- > 0) System.out.println(" i is "+ i); // no curly braces 
-		
-		System.out.println(" Value of i after the loop is "+ i); 
+		int i = 4;
+
+		while (i-- > 0)
+			System.out.println(" i is " + i); // no curly braces
+
+		System.out.println(" Value of i after the loop is " + i);
 	}
-	
+
 	static private void doWhile() {
-		int i = 4; 
-		
-		do System.out.println(" i is "+ i); 
-		while( i--> 0); 
-		
-		System.out.println(" Loop finished. i is "+ i);
+		int i = 4;
+
+		do
+			System.out.println(" i is " + i);
+		while (i-- > 0);
+
+		System.out.println(" Loop finished. i is " + i);
 	}
-	
+
 	static private void myForLoop() {
 		int i = 4;
-		
-		for (System.out.println("Starting loop now"); i > 0; System.out.println(i),i--);
-		
-		System.out.println("Loop finished. i is "+ i);
-		
-		boolean b = false; 
-		for(i = 0 ; b = !b ; ) { System.out.println(i++); }
+
+		for (System.out.println("Starting loop now"); i > 0; System.out.println(i), i--)
+			;
+
+		System.out.println("Loop finished. i is " + i);
+
+		boolean b = false;
+		for (i = 0; b = !b;) {
+			System.out.println(i++);
+		}
 
 	}
 
