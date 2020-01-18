5aa7228803958866a8bd0f6473910561bce9f7d6
nk2164 <nk2164@live.com>
2020-01-18 12:33:57 -0600

    array variables are objects themselves
    Not of the type of elements they hold,but java cooks up its own names
    none the less. Important thing to realize is that they are objects.

--- src/com/nk2164/basics/TriangularMatrix.java
+++ src/com/nk2164/basics/TriangularMatrix.java
@@ -3,13 +3,22 @@ package com.nk2164.basics;
 public class TriangularMatrix {
 
 	public static void main(String[] args) {
-		int k;// j is an int 
-		int[] ia1, ia2; // ia1 and ia2 are one dimensional arrays of ints 
-		int[][] iaa; // iaa is a two dimensional array of ints and so on
-		
-		for (int i = 0,j =0; i < 3; i++) {
-			System.out.printf("(%s,%s = %s) (%s,%s = %s) (%s,%s = %s) \n", i,j,(i+j),i,j+1,(i+j+1),i,j+2,(i+j+2));
+
+		for (int i = 0, j = 0; i < 3; i++) {
+			System.out.printf("(%s,%s = %s) (%s,%s = %s) (%s,%s = %s) \n", i, j, (i + j), i, j + 1, (i + j + 1), i,
+					j + 2, (i + j + 2));
 		}
+
+		int[] ia[] = new int[10][10];
+		boolean[] ba = new boolean[3];
+		String[] stra = new String[5];
+		TestClass[] ta = new TestClass[5];
+
+		System.out.println(ia.getClass().getName() + " , " + ia.getClass().getSuperclass().getName());
+		System.out.println(ba.getClass().getName() + " , " + ba.getClass().getSuperclass().getName());
+		System.out.println(stra.getClass().getName() + " , " + stra.getClass().getSuperclass().getName());
+		System.out.println(ta.getClass().getName() + " , " + ta.getClass().getSuperclass().getName());
+
 	}
 
 }
