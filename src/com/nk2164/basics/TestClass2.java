60edcd29605bc0451405ea7230771fa02e463991
nk2164 <nk2164@live.com>
2020-01-18 16:11:24 -0600

    mismatch and compare array methods

--- src/com/nk2164/basics/TestClass2.java
+++ src/com/nk2164/basics/TestClass2.java
@@ -1,5 +1,7 @@
 package com.nk2164.basics;
 
+import java.util.Arrays;
+
 public class TestClass2 {
 
 	public static String field1 = "Hello World!";
@@ -7,9 +9,18 @@ public class TestClass2 {
 
 	public static void main(String[] args) {
 
-		AccountParent[] sa = {new Account()};
-		AccountParent[] oa = sa;
-		oa[0] = new AccountParent();
-
+//		AccountParent[] sa = {new Account()};
+//		AccountParent[] oa = sa;
+//		oa[0] = new AccountParent();
+//		
+//		Integer[] la = {10,20,30,40};
+//		Object[] ia = la;
+//		ia[0] = new Account();
+		
+		int[] a1 = {10,20,30,40};
+		int[] a2 = {10,20,30,40};
+		
+		System.out.println(Arrays.compare(a1, a2));
+		System.out.println(Arrays.mismatch(a1, a2));
 	}
 }
