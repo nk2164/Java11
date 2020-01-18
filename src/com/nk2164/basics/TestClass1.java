88a093ae51de4e2ed1db460e64990c4bd16deb1b
nk2164 <nk2164@live.com>
2020-01-18 02:53:29 -0600

    Do While loops lables

--- src/com/nk2164/basics/TestClass1.java
+++ src/com/nk2164/basics/TestClass1.java
@@ -9,17 +9,37 @@ public class TestClass1 {
 	}
 
 	public static void main(String[] args) {
-//		Object tempVar = new Object(); // 1
-//		TestClass1 tc = new TestClass1(tempVar); // 2
-//		tempVar = new Object(); // 3
-//		tc.instanceVar = null; // 4
+
+		LABEL_1: for (int i = 0; i < 10; i++) {
+			if (i == 2)
+				System.out.println(2);
+
+			for (int j = 0; j < 10; j++) {
+				if (i == 2)
+					continue LABEL_1; // usage of continue is valid because it refers to an outer loop.
+			}
+		}
+	
+	  doIt(5); 
+
+	}
+
+	static void doIt(int h) {
 		
-		String str1 = null;
-		
-		StringBuilder str = new StringBuilder("Hello ");
-		str.append(str1);
-		
-		System.out.println(str);
+		int x = 1;
+
+		LOOP1: do {
+			LOOP2: for (int y = 1; y < h; y++) {
+				if (y == x)
+					continue;
+
+				if (x * x + y * y == h * h) {
+					System.out.println(" Found " + x + " " + y);
+					break LOOP1;
+				}
+			}
+			x++;
+		} while (x < h);
 	}
 
 }
