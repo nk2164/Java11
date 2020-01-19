78867a8c7d61f305365fbcd1c3ddbbefd83f43d8
nk2164 <nk2164@live.com>
2020-01-19 13:06:39 -0600

    printing a 3d array

--- src/com/nk2164/basics/TestClass1.java
+++ src/com/nk2164/basics/TestClass1.java
@@ -9,23 +9,35 @@ public class TestClass1 {
 	}
 
 	public static void main(String[] args) {
+//
+//		LABEL_1: for (int i = 0; i < 10; i++) {
+//			if (i == 2)
+//				System.out.println(2);
+//
+//			for (int j = 0; j < 10; j++) {
+//				if (i == 2)
+//					continue LABEL_1; // usage of continue is valid because it refers to an outer loop.
+//			}
+//		}
+//	
+//	  doIt(5);
 
-		LABEL_1: for (int i = 0; i < 10; i++) {
-			if (i == 2)
-				System.out.println(2);
+		Object[] iaa[][] = {{{" string is also an object"}}, 
+				           {{null}},
+				           {{new Object(), new Integer(10)}}};
 
-			for (int j = 0; j < 10; j++) {
-				if (i == 2)
-					continue LABEL_1; // usage of continue is valid because it refers to an outer loop.
+		for (int i = 0; i < iaa.length; i++) {
+			for (int j = 0; j < iaa[i].length; j++) {
+				System.out.println(" Element[" + i + "]" + "[" + j + "]" + " contains an array of array of length " + iaa[i].length);
+				for (int k=0;k < iaa[i][j].length; k++) {
+					System.out.println(" Element[" + i + "][" + j + "][" + k + "] contains " + iaa[i][j][k]);
+				}
 			}
 		}
-	
-	  doIt(5); 
-
 	}
 
 	static void doIt(int h) {
-		
+
 		int x = 1;
 
 		LOOP1: do {
