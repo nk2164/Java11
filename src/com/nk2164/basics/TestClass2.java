9493d1ea21d5216ffe1a3b5ef754b6250e612b5b
nk2164 <nk2164@live.com>
2020-01-18 18:24:47 -0600

    Deep dive into Array compare and mismatch method.

--- src/com/nk2164/basics/TestClass2.java
+++ src/com/nk2164/basics/TestClass2.java
@@ -17,10 +17,17 @@ public class TestClass2 {
 //		Object[] ia = la;
 //		ia[0] = new Account();
 		
-		int[] a1 = {10,20,30,40};
-		int[] a2 = {10,20,30,40};
+//		int[] a1 = {111,110,112};
+//		int[] a2 = {111,111,111};
+//		
+//		System.out.println(Arrays.compare(a1, a2));
+//		System.out.println(Arrays.mismatch(a1, a2));
 		
-		System.out.println(Arrays.compare(a1, a2));
-		System.out.println(Arrays.mismatch(a1, a2));
+		int[] ia1 = {0, 1, 2, 6}; 
+		int[] ia2 = {0, 1, 2}; 
+		System.out.println( Arrays.compare( ia1, ia2)); // prints -1
+		System.out.println(Arrays.mismatch(ia1, ia2));
+
+		
 	}
 }
