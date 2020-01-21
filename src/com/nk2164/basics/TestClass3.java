4de5e9180e74c0790dbd53a1c69bfc29a9441386
nk2164 <nk2164@live.com>
2020-01-20 22:59:40 -0600

    get average of an array of values

--- src/com/nk2164/basics/TestClass3.java
+++ src/com/nk2164/basics/TestClass3.java
@@ -7,17 +7,32 @@ public class TestClass3 {
 		for (boolean c : b) {
 			System.out.println(c);
 		}
-		
-		int[][][] nums = new int[1][4][2]; 
-		
-		for( int i = 0; i < nums.length; i ++){ 
-			for( int j = 0; j < nums[ i]. length; j ++){
-				for (int k = 0; k < nums[ i][ j]. length; k ++){ 
-					nums[ i][ j][ k] = i + j + k; 
-					System.out.println("num[" + i +"][" + j +"][" + k +"] = "+ nums[i][j][k]); 
-					} 
-				} 
+
+		int[][][] nums = new int[1][4][2];
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				for (int k = 0; k < nums[i][j].length; k++) {
+					nums[i][j][k] = i + j + k;
+					System.out.println("num[" + i + "][" + j + "][" + k + "] = " + nums[i][j][k]);
+				}
 			}
-				
+		}
+		
+		System.out.println(average(new int[]{20,20,20,20,20}));
+
 	}
+
+	public static double average(int[] values) {
+		/*
+		 * by the way, can you tell what will happen if sum is declared as int? Expect
+		 * questions in the exam that seem to be about one topic but are actually about
+		 * something entirely different.
+		 */
+		int sum = 0;
+		for (int i = 0; i < values.length; i++)
+			sum += values[i];
+		return values.length == 0 ? 0 : sum / values.length;
+	}
+
 }
