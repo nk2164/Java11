8bba0724ab8e9bcaf45adff660c9af3d10db5eab
nk2164 <nk2164@live.com>
2020-01-21 22:31:41 -0600

    method overloading

--- src/com/nk2164/basics/TestClass3.java
+++ src/com/nk2164/basics/TestClass3.java
@@ -18,12 +18,12 @@ public class TestClass3 {
 				}
 			}
 		}
-		
-		System.out.println(average(0,new int[]{20,20,20,20,20}));
+
+		System.out.println(average(0, new int[] { 20, 20, 20, 20, 20 }));
 
 	}
 
-	public static double average(int temp,int...values) {
+	public static double average(int temp, int... values) {
 		/*
 		 * by the way, can you tell what will happen if sum is declared as int? Expect
 		 * questions in the exam that seem to be about one topic but are actually about
@@ -35,4 +35,15 @@ public class TestClass3 {
 		return values.length == 0 ? 0 : sum / values.length;
 	}
 
+	public static double average(String temp, int... values) {
+		/*
+		 * by the way, can you tell what will happen if sum is declared as int? Expect
+		 * questions in the exam that seem to be about one topic but are actually about
+		 * something entirely different.
+		 */
+		int sum = 0;
+		for (int i = 0; i < values.length; i++)
+			sum += values[i];
+		return values.length == 0 ? 0 : sum / values.length;
+	}
 }
