55eb37f88d9ce7eea8a8eda6dfd8695d125ec55b
nk2164 <nk2164@live.com>
2020-01-24 18:36:58 -0600

    Rules for resolving methods

--- src/com/nk2164/basics/TestClass4.java
+++ src/com/nk2164/basics/TestClass4.java
@@ -3,15 +3,16 @@ package com.nk2164.basics;
 public class TestClass4 {
 
 	public static void main(String[] args) {
-		doStuff(10);
-		doStuff((byte)10); //short version is called since its more closer than int
+		doStuff(10); //Auto boxing is considered before
+		             //varargs
 	}
 
-	public static void doStuff(int var) {
-		System.out.println("int called");
+	public static void doStuff(int...var) {
+		System.out.println("var args called");
 	}
 	
-	public static void doStuff(short var) {
-		System.out.println("short called");
+	public static void doStuff(Integer var) {
+		System.out.println("Auto boxed");
 	}
+	
 }
