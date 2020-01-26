4f7918d4a96ca7f85641ba051044d47b76c4fec8
nk2164 <nk2164@live.com>
2020-01-25 19:10:26 -0600

    playing around with instance initializers

--- src/com/nk2164/basics/TestClass5.java
+++ src/com/nk2164/basics/TestClass5.java
@@ -12,8 +12,21 @@ public class TestClass5 {
 	public static void main(String[] args) {
 		{
 			System.out.println("In main method!");
+			{
+			  System.out.println("One more");
+			  {
+				  System.out.println("Another one!");
+			  }
+			}
+
 		}
+
 		new TestClass5();
+		
+		{
+			System.out.println("out side but inside main");
+		}
+		
 		System.out.println();
 		new TestClass5();
 	}
@@ -22,7 +35,7 @@ public class TestClass5 {
 	{
 		System.out.println("Instance init executed!");
 	}
-	
+	// static init block
 	static {
 		System.out.println("Static init executed!");
 	}
