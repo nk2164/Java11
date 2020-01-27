ffce28f8f60116dd1da38b913c55ef2371433139
nk2164 <nk2164@live.com>
2020-01-26 20:15:45 -0600

    experimenting with static variable declares

--- src/com/nk2164/basics/TestClass5.java
+++ src/com/nk2164/basics/TestClass5.java
@@ -3,10 +3,12 @@ package com.nk2164.basics;
 public class TestClass5 {
 	int id;
 	String name;
+	static int var1;
 
 	public TestClass5() {
 		id = 111;
 		name = "dummy";
+		var1 += 20;
 	}
 
 	void reset() {
@@ -17,7 +19,14 @@ public class TestClass5 {
 	}
 
 	public static void main(String[] args) {
+		System.out.println(TestClass5.var1);
+
 		TestClass5 a = new TestClass5();
+
+		System.out.println(TestClass5.var1);
+		new TestClass5();
+		System.out.println(a.var1);
+		
 		a.id = 2;
 		a.name = "amy";
 		a.reset();
