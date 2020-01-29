cd0ddc3d8645d7ccb97288dc9a515565a05f34e7
nk2164 <nk2164@live.com>
2020-01-28 20:31:50 -0600

    Accessing static methods

--- src/com/nk2164/basics/InstanceCounterTest.java
+++ src/com/nk2164/basics/InstanceCounterTest.java
@@ -6,7 +6,8 @@ public class InstanceCounterTest {
 		InstanceCounter ic = new InstanceCounter(); 
 		
 		ic.printCount();// accessing static method through a reference to an instance 
-		new InstanceCounter().printCount(); // accessing static method through an implicit reference to an instance 
+		new InstanceCounter();
+		InstanceCounter.printCount(); // accessing static method through an implicit reference to an instance 
 
 		System.out.println( "InstanceCounter.printCount() " + InstanceCounter.count); // accessing static members using the class name
 
