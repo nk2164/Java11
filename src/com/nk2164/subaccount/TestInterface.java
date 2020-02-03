03b48f902b06fc6bde7a641480ac0186679fa224
nk2164 <nk2164@live.com>
2020-02-02 21:30:58 -0600

    Learning interface rules

---
+++ src/com/nk2164/subaccount/TestInterface.java
@@ -0,0 +1,24 @@
+package com.nk2164.subaccount;
+
+public interface TestInterface {
+
+	public void aMethod();
+	
+	//private methods are ok as long as they are not abstract.
+	private void bMethod() {
+	}
+	
+	/* c method is declared private and is not allowed. You will get the following.
+	 * Illegal modifier for the interface method cMethod;
+	 only public, private, abstract, default, static and strictfp are permitted */
+	
+    //protected void cMethod();
+	
+	//* static method needs an implementation.
+	static void dMethod() {
+	}
+	
+	//* default method needs implementation.
+	default void eMethod() {
+	}
+}
