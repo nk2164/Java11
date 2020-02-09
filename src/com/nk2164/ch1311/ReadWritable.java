7616c65a183634529d0adf48a308cef033589b77
nk2164 <nk2164@live.com>
2020-02-09 16:58:31 -0600

    Variable inheritance rules!

--- src/com/nk2164/ch1311/ReadWritable.java
+++ src/com/nk2164/ch1311/ReadWritable.java
@@ -2,6 +2,9 @@ package com.nk2164.ch1311;
 
 public interface ReadWritable extends Readable, Writable {
 
+	int x = Readable.SIZE + Writable.SIZE; //ReadWritable gets 2 copies of variable 
+	                                       //SIZE from super interfaces.
+
 	@Override
 	default void defaultMethod() {
 		System.out.println("In read writable!!");
