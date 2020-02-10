db7d8b8f85d5b36969b35f7db32f67e5985d79db
nk2164 <nk2164@live.com>
2020-02-10 08:25:24 -0600

    Write a small parameterized class

---
+++ src/com/nk2164/example/DataHolder.java
@@ -0,0 +1,13 @@
+package com.nk2164.example;
+
+public class DataHolder <E> {
+
+	E data;
+	public E getData() {
+		return this.data;
+	}
+	
+	public void setData(E data) {
+		this.data = data;
+	}
+}
