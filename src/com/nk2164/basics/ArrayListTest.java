48009397c4b262c42aa893abf571a1f114b64439
nk2164 <nk2164@live.com>
2020-02-12 21:46:49 -0600

    Learnt about toArray method

--- src/com/nk2164/basics/ArrayListTest.java
+++ src/com/nk2164/basics/ArrayListTest.java
@@ -7,8 +7,20 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 		List<String> ls = new ArrayList<>();
-		ls.add("Hello");
-		String testString = new String("Hello");
-		System.out.println(ls.contains(testString));
+		ls.add("zero");
+		ls.add("one");
+		ls.add("two");
+		ls.add("three");
+		ls.add("four");
+		ls.add("five");
+		ls.add("six");
+		ls.add("seven");
+		
+		System.out.println(ls.size());
+		List<String> subL = ls.subList(8, 8);
+		System.out.println(subL);
+		
+		String[] strArray = ls.toArray(new String[10]);
+		System.out.println(strArray.length);
 	}
 }
