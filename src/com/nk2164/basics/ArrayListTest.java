dc9f470e9e37e96b214053e0dbec3b5452ed3b6f
nk2164 <nk2164@live.com>
2020-02-12 21:08:30 -0600

    Contains looks for string object whose values match

--- src/com/nk2164/basics/ArrayListTest.java
+++ src/com/nk2164/basics/ArrayListTest.java
@@ -6,10 +6,9 @@ import java.util.List;
 public class ArrayListTest {
 
 	public static void main(String[] args) {
-		List<Integer> li = new ArrayList<>();
-		li.add(0, 1);
-		li.add(0, 2);
-		li.add(0, 3);
-		System.out.println(li);
+		List<String> ls = new ArrayList<>();
+		ls.add("Hello");
+		String testString = new String("Hello");
+		System.out.println(ls.contains(testString));
 	}
 }
