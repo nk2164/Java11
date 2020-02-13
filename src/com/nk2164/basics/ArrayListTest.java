2bb5942ee8d51465afbcf4664649c00c721d4e35
nk2164 <nk2164@live.com>
2020-02-12 22:19:51 -0600

    copyOf and listOf methods

--- src/com/nk2164/basics/ArrayListTest.java
+++ src/com/nk2164/basics/ArrayListTest.java
@@ -19,9 +19,12 @@ public class ArrayListTest {
 		List<String> subL = ls.subList(1, 3);
 		System.out.println(subL);
 		
-		List<String> origList = List.of("a", "b", "c");
-		List<String> copyList = List.copyOf(origList);
-		copyList.add("d");
+		List<String> origList = List.of("a", "b", "c"); //Returns an unmodifiable list
+		List<String> copyList = List.copyOf(origList);  //Returns an unmodifiable list
+		
+//		origList.add("d"); - > Can't do this 
+//		copyList.add("d"); - > Can't do this
+		
 		System.out.println(origList);
 		System.out.println(copyList);
 	}
