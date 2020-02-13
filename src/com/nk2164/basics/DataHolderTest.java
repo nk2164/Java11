4c893d465b7d62b0197a5e212ba5b10209fe5e37
nk2164 <nk2164@live.com>
2020-02-12 18:55:20 -0600

    Generics in action

--- src/com/nk2164/basics/DataHolderTest.java
+++ src/com/nk2164/basics/DataHolderTest.java
@@ -6,8 +6,17 @@ public class DataHolderTest {
 
 	public static void main(String[] args) {
 		DataHolder<String> ds = new DataHolder<>();
+		DataHolder<Integer> di = new DataHolder<>();
+		
+		di.setData(2000);
 		ds.setData("Hello World!!");
 		
 		System.out.println(ds.getData());
+		System.out.println(di.getData());
 	}
+	
+	public void processData(DataHolder<String> str) {
+		//do something
+	}
+	
 }
--- src/com/nk2164/example/FunctionTest1.java
+++ src/com/nk2164/example/FunctionTest1.java
@@ -9,7 +9,7 @@ public class FunctionTest1 {
 		return (d < 50);
 	}
 	
-	List<Double> myList = new ArrayList<Double>();
+	static ArrayList<Double> myList = new ArrayList<Double>();
 	
 	public FunctionTest1() {
 		myList.add(10.00);
@@ -19,6 +19,7 @@ public class FunctionTest1 {
 		myList.add(50.00);
 		myList.add(60.00);
 		myList.add(70.00);
+		
 	}
 	
 	private void printMe(Predicate<Double> testCriteria) {
@@ -39,5 +40,8 @@ public class FunctionTest1 {
 		f1.printMe(s -> s >= 50.00);
 		System.out.println();
 		f1.printMe(f1::isItLowerThan50);
+		
+		System.out.println("Printing elements of the list");
+		myList.forEach(System.out::println);
 	}
 }
