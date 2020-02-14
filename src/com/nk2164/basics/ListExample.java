9c6d17d2758e62e28937ae5c6d49f5625f78a5cc
nk2164 <nk2164@live.com>
2020-02-13 21:35:21 -0600

    Removeall method

--- src/com/nk2164/basics/ListExample.java
+++ src/com/nk2164/basics/ListExample.java
@@ -1,15 +1,21 @@
 package com.nk2164.basics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ListExample {
 
 	public static void main(String[] args) {
 		var al = new ArrayList<String>();
-		al.add(0," a");
-		al.add(0," b");
-		al.add(0," c");
+		al.add(0,"a");
+		al.add(0,"b");
+		al.add(0,"c");
 		System.out.println(al);
+		
+		ArrayList<String> am = new ArrayList<>(Arrays.asList(new String[]{"a","b","c","d"}));
+		System.out.println(am);
+		am.removeAll(al);
+		System.out.println(am);
 
 	}
 }
