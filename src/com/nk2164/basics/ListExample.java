4fa1050a592d8602fed4e0fb32c69a55b5c10a77
nk2164 <nk2164@live.com>
2020-02-13 21:18:13 -0600

    Methods in list interface

---
+++ src/com/nk2164/basics/ListExample.java
@@ -0,0 +1,16 @@
+package com.nk2164.basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+		var al = new ArrayList<String>();
+		al.add(0," a");
+		al.add(0," b");
+		al.add(0," c");
+		System.out.println(al);
+
+	}
+}
