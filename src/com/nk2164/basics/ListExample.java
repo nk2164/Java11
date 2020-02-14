aa7b43d68b02d2279ef064eb7da5abecc4f20ee0
nk2164 <nk2164@live.com>
2020-02-13 22:20:30 -0600

    addAll method

--- src/com/nk2164/basics/ListExample.java
+++ src/com/nk2164/basics/ListExample.java
@@ -6,16 +6,17 @@ public class ListExample {
 
 	public static void main(String[] args) {
 
-		var list1 = new ArrayList <String>(); 
-		var list2 = new ArrayList <String>(); 
-		
-		list1.add("a"); list1. add("b"); 
-		list2.add("b"); list2. add("c"); list2.add("d"); 
-		
-		list1.addAll(list2); 
-		list1.remove("b"); 
-		System.out.println( list1);
-
+		ArrayList<String> list1 = new ArrayList<>();
+		ArrayList<String> list2 = new ArrayList<>();
+		list1.add("a");
+		list1.add("b");
+		list2.add("b");
+		list2.add("c");
+		list2.add("d");
+		list1.addAll(list2);
+		System.out.println(list1);
+		list1.remove("b");
+		System.out.println(list1);
 
 	}
 }
