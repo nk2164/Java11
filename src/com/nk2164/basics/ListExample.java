7b18f5f2cc0363ed35de2118aa74b297feaa67fc
nk2164 <nk2164@live.com>
2020-02-14 02:15:01 -0600

    Basic HashMap

--- src/com/nk2164/basics/ListExample.java
+++ src/com/nk2164/basics/ListExample.java
@@ -1,6 +1,8 @@
 package com.nk2164.basics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListExample {
 
@@ -16,6 +18,15 @@ public class ListExample {
 		list1.addAll(list2);
 //		list1.removeAll("b"); - Remove all takes only collection
 		System.out.println(list1);
-
+		
+		Map<Integer, String>  studentDetail = new HashMap<>();
+		studentDetail.put(1, "AB");
+		studentDetail.put(2, "CD");
+		studentDetail.put(3, "EF");
+		studentDetail.put(4, "GH");
+		studentDetail.put(5, "IJ");
+		studentDetail.put(6, "KL");
+		
+		System.out.println(studentDetail.get(5));
 	}
 }
