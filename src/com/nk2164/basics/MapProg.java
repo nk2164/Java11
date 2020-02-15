3331555f7aa8109d4664f51a7c033b41bab33918
nk2164 <nk2164@live.com>
2020-02-15 02:47:31 -0600

    Simple hashmap

---
+++ src/com/nk2164/basics/MapProg.java
@@ -0,0 +1,20 @@
+package com.nk2164.basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProg {
+
+	public static void main(String[] args) {
+		Map<Integer, String> map1 = new HashMap<Integer, String>();
+		map1.put(1, "Hi");
+		map1.put(1, "my");
+		map1.put(1, "name");
+		map1.put(1, "is");
+		map1.put(1, "Isabel");
+		map1.put(1, "Jean");
+		
+		System.out.println(map1.get(1));
+		
+	}
+}
