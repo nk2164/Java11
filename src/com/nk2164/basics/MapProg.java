516ad5e6b35f08334b260f44c46bd5b4ec956963
nk2164 <nk2164@live.com>
2020-02-15 03:10:45 -0600

    Get the keys using the keyset method.

--- src/com/nk2164/basics/MapProg.java
+++ src/com/nk2164/basics/MapProg.java
@@ -1,6 +1,8 @@
 package com.nk2164.basics;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -10,15 +12,16 @@ public class MapProg {
 	public static void main(String[] args) {
 		Map<Integer, String> map1 = new HashMap<Integer, String>();
 		map1.put(1, "Hi");
-		map1.put(1, "my");
-		map1.put(1, "name");
-		map1.put(1, "is");
-		map1.put(1, "Isabel");
-		map1.put(1, "Jean");
+		map1.put(2, "my");
+		map1.put(3, "name");
+		map1.put(4, "is");
+		map1.put(5, "Isabel");
+		map1.put(6, "Jean");
 		
 		System.out.println(map1.get(1));
 		
-		Set<Integer> ks = new TreeSet<>();
+		Set<Integer> keys = map1.keySet();
+		keys.forEach(System.out::println);
 		
 	}
 }
