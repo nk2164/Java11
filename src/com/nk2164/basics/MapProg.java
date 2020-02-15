b24af7c806db57a32a5569ee45170607a74d19bb
nk2164 <nk2164@live.com>
2020-02-15 11:33:10 -0600

    Wrote code that implements various methods in HashMap class

--- src/com/nk2164/basics/MapProg.java
+++ src/com/nk2164/basics/MapProg.java
@@ -1,5 +1,6 @@
 package com.nk2164.basics;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -16,9 +17,15 @@ public class MapProg {
 		map1.put(6, "Jean");
 		
 		System.out.println(map1.get(1));
-		
+		map1.remove(1); // Removed the element with key 1
+		System.out.println(map1.get(1)); // getting return null because 
+										 // that element is not there.	
 		Set<Integer> keys = map1.keySet();
 		keys.forEach(System.out::println);
 		
+		Collection<String> col = map1.values();
+		
+		col.forEach(System.out::println);
+		System.out.println(map1.size());
 	}
 }
