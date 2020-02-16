e59e0900556baf4c6d53ded5715f91cc1b987e42
nk2164 <nk2164@live.com>
2020-02-16 15:04:55 -0600

    Tried the removeif method

--- src/com/nk2164/function/TestClass.java
+++ src/com/nk2164/function/TestClass.java
@@ -1,7 +1,10 @@
 package com.nk2164.function;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -12,11 +15,25 @@ public class TestClass {
 		sc.accept("Hello World!");
 		
 		String[] strArray = {"Experimenting","with","supplier","Interface"};
-
+		
 		System.out.println(getData(() -> new String[] {"Hello","World"}));
 		System.out.println(getData(() -> new String[] {"How","are","you?"}));
 		System.out.println(getData(() -> new String[] {"see","how ","cool","this","is?"}));
 		System.out.println(getData(() -> strArray));
+		
+		Map<Integer, String> myMap = new HashMap<Integer, String>();
+		myMap.put(1, "Hello");
+		myMap.put(2, "World!");
+		myMap.forEach((k,v) -> System.out.println("The key " + k + " is mapped to " + v));
+
+		List<String> myList = new ArrayList<>();
+		myList.add("Experimenting");
+		myList.add("with");
+		myList.add("supplier");
+		myList.add("string");
+		System.out.println(myList);
+		myList.removeIf(s -> s.equals("supplier"));
+		System.out.println(myList);
 		
 	}
 	
