53e749a4ec115437678c49f2d71d1566111cb7fb
nk2164 <nk2164@live.com>
2020-02-16 16:05:23 -0600

    Learn where to use variables used in scope.

--- src/com/nk2164/function/TestClass.java
+++ src/com/nk2164/function/TestClass.java
@@ -6,25 +6,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class TestClass {
-	
+
 	public static void main(String[] args) {
 		Consumer<String> sc = s -> System.out.println(s);
 		sc.accept("Hello World!");
-		
-		String[] strArray = {"Experimenting","with","supplier","Interface"};
-		
-		System.out.println(getData(() -> new String[] {"Hello","World"}));
-		System.out.println(getData(() -> new String[] {"How","are","you?"}));
-		System.out.println(getData(() -> new String[] {"see","how ","cool","this","is?"}));
+
+		String[] strArray = { "Experimenting", "with", "supplier", "Interface" };
+
+		System.out.println(getData(() -> new String[] { "Hello", "World" }));
+		System.out.println(getData(() -> new String[] { "How", "are", "you?" }));
+		System.out.println(getData(() -> new String[] { "see", "how ", "cool", "this", "is?" }));
 		System.out.println(getData(() -> strArray));
-		
+
 		Map<Integer, String> myMap = new HashMap<Integer, String>();
 		myMap.put(1, "Hello");
 		myMap.put(2, "World!");
-		myMap.forEach((k,v) -> System.out.println("The key " + k + " is mapped to " + v));
+		myMap.forEach((k, v) -> System.out.println("The key " + k + " is mapped to " + v));
 
 		List<String> myList = new ArrayList<>();
 		myList.add("Experimenting");
@@ -34,12 +35,53 @@ public class TestClass {
 		System.out.println(myList);
 		myList.removeIf(s -> s.equals("supplier"));
 		System.out.println(myList);
+
+		List<Integer> myIntList = new ArrayList<>();
+		myIntList.add(4);
+		myIntList.add(3);
+		myIntList.add(2);
+		myIntList.add(1);
+		System.out.println(myIntList);
+		myIntList.sort((firstArg, secondArg) -> firstArg.compareTo(secondArg));
+		System.out.println(myIntList);
 		
+		// Reversig the sort - note the - 
+		myIntList.sort((firstArg, secondArg) -> - firstArg.compareTo(secondArg));
+		System.out.println(myIntList);
+
+		List<String> games = new ArrayList<>(List.of("football", "cricket", "baseball", "tennis"));
+		games.sort((a, b) -> a.compareTo(b));
+		games.forEach(s -> System.out.println(s));
+
+		System.out.println();
+		
+		List<String> names = Arrays.asList(new String[] {"alex","bob","casey","abel"});
+		for(String n: names) {
+			Predicate<String> p = q -> q.startsWith("a");
+			if (p.test(n)) {
+				System.out.println(n);
+			}
+		}
+		
+		System.out.println();
+		
+		List<String> names1 = Arrays.asList(new String[] {"alex","bob","casey","abel"});
+		
+		int x = 0;
+		for (String n: names1) {
+			Predicate<String> q = k -> {
+				System.out.println(n);
+				return k.startsWith("a");
+			};
+			
+			System.out.println(q.test(n));
+		}
+		x += 1;
 	}
-	
-	public static List<String> getData(Supplier<String[]> s){
-	  List<String> output = Arrays.asList(s.get());
-	  return output;
+
+	public static List<String> getData(Supplier<String[]> s) {
+		List<String> output = Arrays.asList(s.get());
+		return output;
 	}
 
 }
