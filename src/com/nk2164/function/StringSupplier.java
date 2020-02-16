38e83486d3a7d303b4174b48d1965ffcf55108ba
nk2164 <nk2164@live.com>
2020-02-16 14:33:08 -0600

    A Simple supplier interface

---
+++ src/com/nk2164/function/StringSupplier.java
@@ -0,0 +1,14 @@
+package com.nk2164.function;
+
+import java.util.function.Supplier;
+
+public class StringSupplier implements Supplier<String> {
+
+	@Override
+	public String get() {
+		
+		return null;
+	}
+
+	
+}
--- src/com/nk2164/function/TestClass.java
+++ src/com/nk2164/function/TestClass.java
@@ -1,12 +1,28 @@
 package com.nk2164.function;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class TestClass {
 	
 	public static void main(String[] args) {
 		Consumer<String> sc = s -> System.out.println(s);
 		sc.accept("Hello World!");
+		
+		String[] strArray = {"Experimenting","with","supplier","Interface"};
+
+		System.out.println(getData(() -> new String[] {"Hello","World"}));
+		System.out.println(getData(() -> new String[] {"How","are","you?"}));
+		System.out.println(getData(() -> new String[] {"see","how ","cool","this","is?"}));
+		System.out.println(getData(() -> strArray));
+		
+	}
+	
+	public static List<String> getData(Supplier<String[]> s){
+	  List<String> output = Arrays.asList(s.get());
+	  return output;
 	}
 
 }
