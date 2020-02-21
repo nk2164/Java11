b4e5f60045ae49194182a5c6f5195e88085fbc68
nk2164 <nk2164@live.com>
2020-02-21 01:33:36 -0600

    Throwing a Basic Error

---
+++ src/com/nk2164/basics/ExceptionCheck.java
@@ -0,0 +1,8 @@
+package com.nk2164.basics;
+
+public class ExceptionCheck {
+	public static void main(String[] args) {
+		System.out.println("Throwing a new error :)");
+		throw new Error("Ok I cant go on any further!!");
+	}
+}
