97dfa385481d88c5331cf645db3859d5702d9301
nk2164 <nk2164@live.com>
2020-02-23 14:22:46 -0600

    simple test class

--- src/com/nk2164/basics/TestClass.java
+++ src/com/nk2164/basics/TestClass.java
@@ -1,18 +1,21 @@
 package com.nk2164.basics;
 
-class TestClass{ 
-    static final int value; // not initializing here 
+class TestClass {
+	static final int value; // not initializing here
+	static int i = 5;
 	
-	static { 
-	  value = 2;
+	static{value=2;}
+
+	TestClass() {
+	}
+
+	TestClass(int x) {
+	}
+
+	TestClass(int a, int b) {
 	}
 	
-    TestClass(){ 
-    } 
-    
-    TestClass(int x){ 
-     } 
-    
-    TestClass(int a,int b){ 
-    }
+	public static void main(String[] args) {
+		
+	}
 }
