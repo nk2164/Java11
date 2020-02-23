90576a2e86f245871f2be5c946d3560e0fac6237
nk2164 <nk2164@live.com>
2020-02-23 13:22:59 -0600

    Class that does a basic shift operation.

---
+++ src/com/nk2164/basics/ShiftOperator.java
@@ -0,0 +1,10 @@
+package com.nk2164.basics;
+
+public class ShiftOperator {
+
+	public static void main(String[] args) {
+		int x = 20;
+		System.out.println(x >> 1); //Right shift - Divides by 2
+		System.out.println(x << 1); //Left shift  - Multiplies by 2		
+	}
+}
