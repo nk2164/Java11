77856392fc7f33ae4e56b3fcd60b6af781077604
nk2164 <nk2164@live.com>
2020-03-10 21:27:17 -0500

    basic program to collect item details and print it

--- src/com/d84u/ocjp11/Test.java
+++ src/com/d84u/ocjp11/Test.java
@@ -1,14 +1,72 @@
 package com.d84u.ocjp11;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
-		byte day = 10;
-		switch (day + 1) { //now its ok since day + 1 is an int due to numeric promotion .
-		case 300:
-			System.out.println("BUY 2 GET 1 FREE");
-			break;
-		default:
-			System.out.println("SORRY!!! NO SALE");
+		String itemName;
+		Double itemPrice;
+		String itemOnSale;
+		String moreItems;
+
+		List<Item> items = new ArrayList<>();
+
+		try (Scanner scan = new Scanner(System.in)) {
+			do {
+				// Name of the item
+				System.out.println("Item Name ?");
+				itemName = scan.next();
+
+				// Price of the item
+				System.out.println("Item Price ?");
+				itemPrice = scan.nextDouble();
+
+				// Whether item is on sale
+				System.out.println("On sale?(Yes/No)");
+				itemOnSale = scan.next();
+
+				System.out.println("Have more items ? (Yes/No)");
+				moreItems = scan.next();
+
+				items.add(new Item(itemName, itemPrice, itemOnSale));
+
+			} while (moreItems.equalsIgnoreCase("yes"));
+		} finally {
+			printBill(items);
 		}
+
 	}
+
+	private static void printBill(List<Item> items) {
+		double total = 0.00;
+
+		System.out.println("**************  Grocery Bill  ******************");
+
+		System.out.println("*** ITEM NAME **** ITEM PRICE **** ON SALE ? ****");
+
+		for (Item item : items) {
+			System.out.printf("    %s             %.2f           %s           \n", item.itemName, item.price,
+					item.itemOnSale);
+			total += item.price;
+		}
+
+		System.out.println("************************************************");
+		System.out.println("Total :" + total);
+	}
+}
+
+class Item {
+
+	String itemName;
+	double price;
+	String itemOnSale;
+
+	public Item(String itemName, double price, String itemOnSale) {
+		this.itemName = itemName;
+		this.price = price;
+		this.itemOnSale = itemOnSale;
+	}
+
 }
