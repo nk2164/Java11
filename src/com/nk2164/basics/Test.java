df53e688bcf07ce3493e86ff8ae15646e05716dc
nk2164 <nk2164@live.com>
2020-03-15 20:01:00 -0500

    expressions

--- src/com/nk2164/basics/Test.java
+++ src/com/nk2164/basics/Test.java
@@ -1,20 +1,14 @@
 package com.nk2164.basics;
 
-class Test {
-
+public class Test {
 	public static void main(String[] args) {
-		for (var i = 1; i < 11; i++) {
-			System.out.println("Count is: " + i);
-		}
-
-		System.out.println();
-		
-		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		
-		for (var item : numbers) {
-			System.out.println("Count is: " + item);
+		for (int x = 10, y = 11, z = 12; y > x && z > y; y++, z -= 2) {
+			System.out.println(x + y + z);
 		}
 		
-//		var a = 2, b = 3; - will fail with message 'var' is not allowed in a compound declaration
+		int y;
+		int z;
+		int x = 2 + (y = 2 + (z = 3 + 5)); //learning more about expressions
+		System.out.println(x);
 	}
 }
