12be3146f874028a06765c048e618ed5a4d506ac
nk2164 <nk2164@live.com>
2020-03-15 21:52:07 -0500

    expressions

--- src/com/d84u/ocjp11/Test.java
+++ src/com/d84u/ocjp11/Test.java
@@ -1,72 +1,8 @@
 package com.d84u.ocjp11;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Test {
 	public static void main(String[] args) {
-		String itemName;
-		Double itemPrice;
-		String itemOnSale;
-		String moreItems;
-
-		List<Item> items = new ArrayList<>();
-
-		try (Scanner scan = new Scanner(System.in)) {
-			do {
-				// Name of the item
-				System.out.println("Item Name ?");
-				itemName = scan.next();
-
-				// Price of the item
-				System.out.println("Item Price ?");
-				itemPrice = scan.nextDouble();
-
-				// Whether item is on sale
-				System.out.println("On sale?(Yes/No)");
-				itemOnSale = scan.next();
-
-				System.out.println("Have more items ? (Yes/No)");
-				moreItems = scan.next();
-
-				items.add(new Item(itemName, itemPrice, itemOnSale));
-
-			} while (moreItems.equalsIgnoreCase("yes"));
-		} finally {
-			printBill(items);
-		}
-
+		int var = 128;
+		System.out.println(var = var - (var - 2));
 	}
-
-	private static void printBill(List<Item> items) {
-		double total = 0.00;
-
-		System.out.println("**************  Grocery Bill  ******************");
-
-		System.out.println("*** ITEM NAME **** ITEM PRICE **** ON SALE ? ****");
-
-		for (Item item : items) {
-			System.out.printf("    %s             %.2f           %s           \n", item.itemName, item.price,
-					item.itemOnSale);
-			total += item.price;
-		}
-
-		System.out.println("************************************************");
-		System.out.println("Total :" + total);
-	}
-}
-
-class Item {
-
-	String itemName;
-	double price;
-	String itemOnSale;
-
-	public Item(String itemName, double price, String itemOnSale) {
-		this.itemName = itemName;
-		this.price = price;
-		this.itemOnSale = itemOnSale;
-	}
-
 }
