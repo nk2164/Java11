9e415f5ae891e58718888d3e589af8227941aca6
nk2164 <nk2164@live.com>
2020-03-18 01:41:00 -0500

    small updates

--- src/com/d84u/ocjp11/Test.java
+++ src/com/d84u/ocjp11/Test.java
@@ -2,7 +2,16 @@ package com.d84u.ocjp11;
 
 public class Test {
 	public static void main(String[] args) {
-		int var = 128;
-		System.out.println(var = var - (var - 2));
+		byte var = 100;
+		switch (var) {
+		case 100:
+			System.out.println("var is 100");
+			break;
+		case 12:
+			System.out.println("var is 200");
+			break;
+		default:
+			System.out.println("In default");
+		}
 	}
 }
