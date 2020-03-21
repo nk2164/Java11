059883ad5a4dc8175e3db952d794db92be0863a4
nk2164 <nk2164@live.com>
2020-03-21 18:13:28 -0500

    simple sorts using a comparator

--- src/com/d84u/ocjp11/Test.java
+++ src/com/d84u/ocjp11/Test.java
@@ -1,17 +1,44 @@
 package com.d84u.ocjp11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
-	public static void main(String[] args) {
-		byte var = 100;
-		switch (var) {
-		case 100:
-			System.out.println("var is 100");
-			break;
-		case 12:
-			System.out.println("var is 200");
-			break;
-		default:
-			System.out.println("In default");
+
+	public static class Book {
+		int itemNumber;
+		String title;
+
+		public Book(int itemNumber, String title) {
+			this.itemNumber = itemNumber;
+			this.title = title;
 		}
+
+		@Override
+		public String toString() {
+			return this.title;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		List<Book> books = new ArrayList<Book>();
+		books.add(new Book(1, "The Lord of the rings"));
+		books.add(new Book(1, "Le Petit"));
+		
+		System.out.println(books.toString());
+
+		Collections.sort(books, new Comparator<Book>() {
+
+			@Override
+			public int compare(Book o1, Book o2) {
+				return o1.title.compareTo(o2.title);
+			}
+
+		});
+		
+		System.out.println(books.toString());
 	}
 }
