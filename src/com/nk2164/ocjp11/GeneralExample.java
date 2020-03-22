c1d9e137f7bfced7049e0e47a1078b7a8ea5d3c0
nk2164 <nk2164@live.com>
2020-03-22 15:15:38 -0500

    Using a simple Stream

--- src/com/nk2164/ocjp11/GeneralExample.java
+++ src/com/nk2164/ocjp11/GeneralExample.java
@@ -1,8 +1,25 @@
 package com.nk2164.ocjp11;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 public class GeneralExample {
+	
+	public List<Integer> books = null;
+	
+	public GeneralExample() {
+		books = new ArrayList<Integer>();
+		
+		books.add(1);
+		books.add(2);
+		books.add(3);
+		books.add(4);
+		books.add(5);
+		books.add(6);
+		books.add(7);
+	}
 
 	//Define a function that takes a built in functional interface
 	public static void doSomething(BiConsumer<String, Integer> someLambda, String t,Integer i) {
@@ -30,5 +47,12 @@ public class GeneralExample {
 		doSomething(anotherLambda, "John", 10);
 		doSomething(anotherLambda, "Greg", 10);
 		
+		System.out.println(getTotal(new GeneralExample().books));
+	}
+	
+	public static Integer getTotal(List<Integer> books) {
+		
+		Stream<Integer> bookStream = books.stream();
+		return bookStream.reduce(0,(a,b) -> a+b);
 	}
 }
