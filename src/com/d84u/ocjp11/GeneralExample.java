51898adddb67b64ec770c7224d727f58063da64a
nk2164 <nk2164@live.com>
2020-03-22 14:09:24 -0500

    Do the same thing using a built in functional interface

--- src/com/d84u/ocjp11/GeneralExample.java
+++ src/com/d84u/ocjp11/GeneralExample.java
@@ -1,21 +1,18 @@
 package com.d84u.ocjp11;
 
+import java.util.function.BiConsumer;
+
 public class GeneralExample {
-	//Define the interface
-	@FunctionalInterface
-	interface LambdaInterface {
-		void someMethod(String s,Integer i);
-	}
-	
-	//Define a method that would take in that interface
-	public static void doSomething(LambdaInterface someLambda,String s, Integer i) {
-		someLambda.someMethod(s, i);
+
+	//Define a function that takes a built in functional interface
+	public static void doSomething(BiConsumer<String, Integer> someLambda, String t,Integer i) {
+		someLambda.accept(t, i);
 	}
 
 	public static void main(String[] args) {
 		
 		//provide an implementation using lambda expression
-		LambdaInterface someLambda = (s , i) -> {
+		BiConsumer<String,Integer> someLambda = (s , i) -> {
 			System.out.println("Hello " + s + " !");
 			System.out.println(s + " has " + i + " teeth!");
 		};
@@ -25,7 +22,7 @@ public class GeneralExample {
 		
 		System.out.println();
 		
-		LambdaInterface anotherLambda = (s , i) -> {
+		BiConsumer<String,Integer> anotherLambda = (s , i) -> {
 			System.out.println("My name is " + s + " !");
 			System.out.println("I have " + i + " teeth!");
 		};
