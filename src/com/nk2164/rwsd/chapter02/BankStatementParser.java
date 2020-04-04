951f8533041c53c0291f21cad5a577354e966db1
nk2164 <nk2164@live.com>
2020-04-04 16:41:58 -0500

    Bank Transaction simple

---
+++ src/com/nk2164/rwsd/chapter02/BankStatementParser.java
@@ -0,0 +1,8 @@
+package com.nk2164.rwsd.chapter02;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
